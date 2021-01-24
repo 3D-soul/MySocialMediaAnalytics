114592362bba6876eac169f83e9df07d4babbafb
3D-soul <suhruthyambakam822001@gmail.com>
2021-01-24 18:56:07 +0530

    completed scraping teamspeed data

---
+++ Python/Scrapy/teamspeed/teamspeed/pipelines.py
@@ -0,0 +1,50 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+from pymongo import MongoClient
+import sqlite3
+
+class TeamspeedPipeline:
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+        self.client = MongoClient("mongodb://127.0.0.1:27017/")
+        self.collection = self.client["scrapy-db"].teamspeed
+
+    def create_connection(self):
+        self.conn = sqlite3.connect("./data/teamspeed.db")
+        self.cur = self.conn.cursor()
+
+    def create_table(self):
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS teamspeed_data(
+                header text,
+                author text,
+                last_post text,
+                replies text,
+                views text            
+        ) """)
+
+
+    def process_item(self, item, spider):
+        self.store_db(item)
+
+    def store_db(self, item):
+        a, b, c, d, e = item["header"], item["author"], item["last_post"], item["replies"], item["views"]
+
+        self.cur.execute("""INSERT INTO teamspeed_data VALUES (?,?,?,?,?)""", (a, b, c, d, e))
+        self.conn.commit()
+
+        self.collection.insert({
+            "header": a,
+            "author": b,
+            "last_post": c,
+            "replies": d,
+            "views":e
+        })
---
+++ Python/Scrapy/teamspeed/teamspeed/spiders/forum_teamspeed.py
@@ -0,0 +1,47 @@
+from ..items import TeamspeedItem
+from bs4 import BeautifulSoup as bs
+import scrapy
+import re
+
+class ForumTeamspeedSpider(scrapy.Spider):
+    name = "teamspeed"
+    page_num=2
+    start_urls = [
+        "https://teamspeed.com/forums/gadgets-electronics-home-theater-gaming/?daysprune=-1"
+    ]
+
+    def parse(self, response):
+        items = TeamspeedItem()
+
+        soup = bs(response.text, "html.parser")
+        all_posts = soup.find_all("div", {"class":"trow text-center"})
+
+        for post in all_posts:
+            header = post.select("h4 a")[0].getText()
+            author = [tag.getText() for tag in post.find_all("a", {"rel":"nofollow"})][0]
+            try:
+                last_post = post.find("div", {"class":"smallfont text-right"}).getText()
+                last_post =  " ".join(re.sub(r'[\t\r\n]', ' ', last_post).split())
+            except:
+                last_post = None
+            try:
+                replies = [tag.getText() for tag in post.find_all("a", {"rel":"nofollow"})][1]
+            except:
+                replies = None
+            try:
+                views =  post.select("div span")[-1].getText()
+            except:
+                views = None
+
+            items["header"] = header
+            items["author"] = author
+            items["last_post"] = last_post
+            items["replies"] = replies
+            items["views"] = views
+
+            yield items
+
+        next_page = f"https://teamspeed.com/forums/gadgets-electronics-home-theater-gaming/index{ForumTeamspeedSpider.page_num}.html?daysprune=-1"
+        if ForumTeamspeedSpider.page_num <=35:
+            yield response.follow(next_page, callback=self.parse)
+            ForumTeamspeedSpider.page_num +=1
